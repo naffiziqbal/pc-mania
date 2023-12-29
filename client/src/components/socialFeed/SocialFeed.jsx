33a5fec3c2bca8e144394df6b10d7b0eeb8cfcf7
naffiziqbal <f.nafiziqbal@gmail.com>
2023-12-29 10:59:32 +0600

    Instagram Feed

---
+++ client/src/components/socialFeed/SocialFeed.jsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const SocialFeed = () => {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        const getPost = async () => {
+            const data = await fetch('/db.json')
+            const posts = await data.json()
+            setPosts(posts)
+        }
+        getPost()
+    }, [])
+    return (
+        <div>
+            <h3 className="my-3 font-bold text-xl"> Follow us on Instagram for news and Offers and more</h3>
+            <div className="grid grid-cols-4 gap-5">
+                {
+                    posts?.slice(0, 6).map(post => <div key={post._id}>
+                        <div className="border rounded p-3 min-h-fit min-w-fit hover:border-blue-400 hover:scale-105 duration-300">
+                            <Image src={post.image}
+                                className="min-h-52 w-fit"
+
+                                width={100}
+                                height={100}
+                                alt="img" />
+                            <p className="text-center text-sm font-semibold">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet repudiandae quaerat labore autem, totam tenetur blanditiis itaque dolorum delectus modi quidem vero, ipsam, et ea dignissimos explicabo quis quas eos!</p>
+                        </div>
+                    </div>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SocialFeed;
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,7 +2,7 @@ import Slider from "@/components/ui/Slider/Slider";
 import Products from "@/components/products/Products";
 import CategoryWiseProduct from "@/components/products/CategoryWiseProduct";
 import Brands from "@/components/brands/Brands";
-
+import SocialFeed from "@/components/socialFeed/SocialFeed";
 
 const Home = ({ products }) => {
   console.log(products)
@@ -12,6 +12,7 @@ const Home = ({ products }) => {
     <Slider />
     <Products products={products} />
     <Brands />
+    <SocialFeed />
 
   </div>;
 };
