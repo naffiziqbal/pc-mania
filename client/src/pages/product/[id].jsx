d570d655b16d20b6d0463a52025efa88f3565c6b
naffiziqbal <f.nafiziqbal@gmail.com>
2023-12-29 21:54:02 +0600

    Basic Design Product Details Page

--- client/src/pages/product/[id].jsx
+++ client/src/pages/product/[id].jsx
@@ -1,26 +1,40 @@
+import Button from '@/components/ui/Button/Button';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const ProductDetails = ({ product }) => {
     const { data } = product
-    const { name, image, description, price } = data
+    const [quantity, setQuantity] = useState(1)
+    const { _id, name, image, description, price } = data
     const [userReview, setUserReview] = useState([])
-    // useEffect(() => {
-    //     const getUserReview = async () => { 
-
-    //     }
-    // }, [])
+    useEffect(() => {
+        const getUserReview = async () => {
+            const data = await fetch(`${process.env.SERVER_URL}/review/${_id}`)
+            const review = await data.json()
+            setUserReview(review)
+        }
+        getUserReview()
+    }, [_id])
     console.log(data)
 
     return (
-        <div>
-            <Image src={data.image} width={300} height={400} alt='' />
-            <div>
-                <h3>{name}</h3>
-                <div>
-                    review Secttion
+        <div className='flex flex-row'>
+            <Image src={image} width={300} height={400} alt='' />
+            <div className='w-full'>
+                <h3 className='text-3xl'>{name}</h3>
+                <div className='h-24  w-full pt-2'>
+                    review Section
                 </div>
-                <p>{description}</p>
+                <hr />
+                <p className='mt-4 text-4xl'>${price}</p>
+                <div>
+                    <label htmlFor="quantity" className='w-fit h-fit bg-[#f5f7f7] p-3'>Quantity</label>
+                    <input type="number" value={quantity} onChange={() => setQuantity(quantity + 1)} className='border mt-12 p-2 outline-none mr-12' />
+                    <div className='flex'>
+                        <Button data={"Add To Cart"} />
+                        <Button data={"Buy Now"} />
+                    </div>
+                </div>
             </div>
         </div >
     );
