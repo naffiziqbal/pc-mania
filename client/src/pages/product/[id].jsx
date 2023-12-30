4f64396cb3d534bd9909d2bfd067227c9b68b28d
naffiziqbal <f.nafiziqbal@gmail.com>
2023-12-30 08:29:52 +0600

    Details Page Styling

--- client/src/pages/product/[id].jsx
+++ client/src/pages/product/[id].jsx
@@ -7,33 +7,40 @@ const ProductDetails = ({ product }) => {
     const [quantity, setQuantity] = useState(1)
     const { _id, name, image, description, price } = data
     const [userReview, setUserReview] = useState([])
-    useEffect(() => {
-        const getUserReview = async () => {
-            const data = await fetch(`${process.env.SERVER_URL}/review/${_id}`)
-            const review = await data.json()
-            setUserReview(review)
-        }
-        getUserReview()
-    }, [_id])
+    // useEffect(() => {
+    //     const getUserReview = async () => {
+    //         const data = await fetch(`${process.env.SERVER_URL}/review/${_id}`)
+    //         const review = await data.json()
+    //         setUserReview(review)
+    //     }
+    //     getUserReview()
+    // }, [_id])
     console.log(data)
 
     return (
-        <div className='flex flex-row'>
-            <Image src={image} width={300} height={400} alt='' />
-            <div className='w-full'>
+        <div className='flex md:flex-row flex-col w-full justify-between'>
+            <Image className='w-fit max-w-1/2' src={image} width={400} height={400} layout='fixed' alt='' />
+            <div className='w-1/2'>
                 <h3 className='text-3xl'>{name}</h3>
                 <div className='h-24  w-full pt-2'>
                     review Section
                 </div>
                 <hr />
                 <p className='mt-4 text-4xl'>${price}</p>
-                <div>
+                <div className='flex items-center mt-5'>
                     <label htmlFor="quantity" className='w-fit h-fit bg-[#f5f7f7] p-3'>Quantity</label>
-                    <input type="number" value={quantity} onChange={() => setQuantity(quantity + 1)} className='border mt-12 p-2 outline-none mr-12' />
-                    <div className='flex'>
-                        <Button data={"Add To Cart"} />
-                        <Button data={"Buy Now"} />
+                    <div className='flex items-center'>
+                        <input type="number" value={quantity} className='p-3 border outline-none' />
+                        <button className=' w-fit h-fit border text-2xl font-bold py-2 px-5 hover:bg-[#f5f5f7]  duration-300*:
+                        mx-2'
+                            onClick={() => setQuantity(quantity - 1)}
+                        >-</button>
+                        <button className=' w-fit h-fit border text-xl font-bold py-2 px-5 hover:bg-[#f5f5f7] duration-300 *:
+                        mx-2'
+                            onClick={() => { setQuantity(quantity + 1) }}
+                        >+</button>
                     </div>
+
                 </div>
             </div>
         </div >
